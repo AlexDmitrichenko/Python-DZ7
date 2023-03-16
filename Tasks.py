b461d3fb9dd7b9b81e82f0e702b74f0152399f30
Alex <best1972@list.ru>
2023-03-16 11:24:02 +0300

    recess

--- Tasks.py
+++ Tasks.py
@@ -7,18 +7,48 @@
 # с ритмом все не в порядке 
 # Ввод: пара-ра-рам рам-пам-папам па-ра-па-дам    Вывод: Парам пам-пам
 
-stryng = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
+# stryng = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
 
-text = stryng.split()
-listRes = []
+# text = stryng.split()
+# listRes = []
 
-for word in text:
-    sumVowels = 0
-    for i in word:
-        if i in 'аеиоуыэюя':
-            sumVowels += 1
-    listRes.append(sumVowels)
+# for word in text:
+#     sumVowels = 0
+#     for i in word:
+#         if i in 'аеиоуыэюя':
+#             sumVowels += 1
+#     listRes.append(sumVowels)
     
-print(f'Парам пам-пам' if len(listRes) == listRes.count(listRes[0]) else 'Пам парам')
+# print(f'Парам пам-пам' if len(listRes) == listRes.count(listRes[0]) else 'Пам парам')
 
+# ******************************************************************************************
+# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и 
+# столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые 
+# должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). 
+# Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, 
+# например, у операции умножения.
+# Ввод: print_operation_table(lambda x, y: x * y) 
+# Вывод:                                                                                   
+# 1 2 3 4 5 6
+# 2 4 6 8 10 12
+# 3 6 9 12 15 18
+# 4 8 12 16 20 24
+# 5 10 15 20 25 30
+# 6 12 18 24 30 36
 
+# def printOperationTable(operation, numRows=6, numColumns=6):
+#     a = [[operation(i,j) for j in range(1,numColumns+1)] for i in range(1,numRows+1)]
+#     print(a) 
+#     for i in a:
+#         print(*[f"{x:>3}" for x in i])
+
+# printOperationTable (lambda x, y: x * y)
+# def print_operation_table(operation, num_rows=9, num_columns=9):
+#     for i in range(1, num_rows + 1):
+#         answer = []
+#         for j in range(1, num_columns + 1):
+#             answer.append(str(operation(i, j)))
+#         print(''.join(f'{e:<4}' for e in answer))
+
+# print_operation_table(lambda x, y: x * y)
